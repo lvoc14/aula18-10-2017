ede0ac2fb7e99a3e5d9ef4e9046e49d0637b6aa0
leonardo victor <leonardovictor5@hotmail.com>
2017-10-18 20:09:59 -0300

    Criação de classe Sala e Sessao

---
+++ WebApplication/Models/Sala.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Sala
+    {
+        public int SalaId { get; set; }
+        [Required]
+        public int numero { get; set; }
+        [Required]
+        public int Capacidade { get; set; }
+        [Required]
+
+        public string Descricao { get; set; }
+        //Propriedade de Navegação
+        List<Sessao> Sessoes { get; set; }    
+    }
+}
---
+++ WebApplication/Models/Sessao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    [Table("TbSessao")]
+    public class Sessao
+    {
+        [Key]
+        public int Codigo { get; set; }
+        [Required]
+
+        public DateTime DataHoraInicio { get; set; }
+        [Required]
+
+        public DateTime DataHoraFim { get; set; }
+        [Required]
+        public decimal ValorInteira { get; set; }
+        [Required]
+        public decimal ValorMeia { get; set; }
+        [Required]
+        public bool Encerrada { get; set; }
+
+        public int SalaId { get; set; }
+
+        public Sala Sala { get; set; }
+
+    }
+}
